25b1d92f03b9eae0ddb46249dbddfa4d754c18bd
Amit Rathore <amitrathore@gmail.com>
2010-12-05 23:41:03 -0800

    adding some dependencies

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject Grassroots "1.0.0-SNAPSHOT"
+  :description "Grassroots"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.clojars.amit/smack "3.1.0"]
+                 [org.clojars.amit/smackx "3.1.0"]
+                 [org.clojars.amit/smackx-jingle "3.1.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :disable-implicit-clean true
+  :min-lein-version "1.3.0")

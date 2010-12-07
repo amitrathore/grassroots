ff33d8cf83c87a9f961bb47b86b42c8badaf0d9b
Amit Rathore <amitrathore@gmail.com>
2010-12-07 00:45:51 -0800

    adding db migrations stuff

---
+++ db/migrations/20101207080632_users_table.rb
@@ -0,0 +1,17 @@
+class UsersTable < ActiveRecord::Migration
+  def self.up
+    create_table :users, :force => true do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :encrypted_password
+      t.string :salt
+    end
+    add_index :users, :email, :unique => true
+
+  end
+
+  def self.down
+    drop_table :users
+  end
+end

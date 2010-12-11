bfbf4e78974c2ab7fb50d8468e102ef937d6cac3
Amit Rathore <amitrathore@gmail.com>
2010-12-10 22:36:42 -0800

    playing with cgrand's moustache (not literally, of course)

--- src/com/curry/grassroots/server/core.clj
+++ src/com/curry/grassroots/server/core.clj
@@ -1,1 +1,13 @@
-(ns com.curry.grassroots.server.core)
+(ns com.curry.grassroots.server.core
+  (:use net.cgrand.moustache)
+  (:use com.curry.grassroots.server.users.core)
+  (:use [ring.adapter.jetty :only [run-jetty]])
+  (:use com.curry.grassroots.server.users.core))
+
+(declare grassroots-app)
+
+;(def server (doto (Thread. #(run-jetty #'grassroots-app {:port 8080})) .start))
+
+(def grassroots-app (app
+                     :post handle-post
+                     :get handle-get))
--- src/com/curry/grassroots/server/users/core.clj
+++ src/com/curry/grassroots/server/users/core.clj
@@ -1,2 +1,12 @@
 (ns com.curry.grassroots.server.users.core)
 
+(defn new-user [req]
+  (println "NEW:" req)
+  "hello" (:username req))
+
+(defn handle-post [req]
+  {:body "from POST"})
+
+(defn handle-get [req]
+  (println "getting")
+  {:body "from GET"})
